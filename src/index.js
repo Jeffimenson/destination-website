14fc09ac880c5645b3adbdee4a7956726d7693d6
Jeffimenson <jeffengim@gmail.com>
2023-12-11 17:23:39 -0500

    Have slider be functional, make header disappear when scrolling down, and do some minor styling alterations

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import { query, make } from './jeffQuery.js';
 
 const slider = query('.slider');
+const sliderForward = query('.arrow-holder .forward');
+const sliderBack = query('.arrow-holder .back');
 
 function createCard(title, desc) {
     const card = make('div.card', slider);
@@ -19,10 +21,60 @@ function createCard(title, desc) {
     p.textContent = desc;
 }
 
-const cardCount = 9;
-
-for (let i = 0; i < cardCount; i += 1) {
-    createCard('Test', 'This is the description for the test card');
+function createCards(count) {
+    for (let i = 0; i < count; i += 1) {
+        createCard('Test', 'This is the description for the test card');
+    }
 }
 
-// slider.style.width = `${cardCount * 300}px`;
+function updateSliderButton(canSlide, button) {
+    if (canSlide) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+
+const cardCount = 16;
+const cardsPerSlide = 4;
+const maxSlides = cardCount / cardsPerSlide;
+const minSlides = 0;
+
+createCards(cardCount);
+slider.style.width = `calc(100% * ${maxSlides})`;
+
+let currSlide = 0;
+
+updateSliderButton(currSlide < maxSlides - 1, sliderForward);
+updateSliderButton(currSlide > minSlides, sliderBack);
+sliderForward.addEventListener('click', () => {
+    if (currSlide < maxSlides - 1) {
+        // We subtract one from maxSlides cause if we don't, user can slide out into emptiness
+        currSlide += 1;
+        slider.style.right = `${currSlide * 100}%`;
+    }
+
+    updateSliderButton(currSlide < maxSlides - 1, sliderForward);
+    updateSliderButton(currSlide > minSlides, sliderBack);
+});
+sliderBack.addEventListener('click', () => {
+    if (currSlide > minSlides) {
+        currSlide -= 1;
+        slider.style.right = `${currSlide * 100}%`;
+    }
+
+    updateSliderButton(currSlide > minSlides, sliderBack);
+    updateSliderButton(currSlide < maxSlides - 1, sliderForward);
+});
+
+const header = query('header');
+
+let lastKnownScrollPosition;
+document.addEventListener('scroll', () => {
+    lastKnownScrollPosition = window.scrollY;
+    if (lastKnownScrollPosition > 100) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+});
